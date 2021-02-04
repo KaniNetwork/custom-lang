4a9f0f3b6a5b32f464752a0bf8f5ed90e6a15805
NewGlace <35409106+NewGlace@users.noreply.github.com>
2021-02-04 22:14:35 +0100

    Create description_game.js

---
+++ example/description_game.js
@@ -0,0 +1,124 @@
+/*
+Ne pas modifier le nom de la clé !
+Modifier seulement la valeur de cette clé
+
+Exemple: 
+
+"Clé" : "Valeur"
+*/ 
+
+const WerewolfTemplate = {
+    "CHANNEL_LG": {
+        "Category" : "• Partie de Loup-garou •",
+        "Village" : "・Village",
+        "Cupidon": "・Cupidon", 
+        "Voyante": "・Voyante", 
+        "Voyante_bavard": "・Voyante Bavard", 
+        "Renard": "・Renard", 
+        "Oracle": "・Oracle", 
+        "Orcale_oeil": "・Oracle au 3e oeil", 
+        "Montreur": "・Montreur d'ours", 
+        "Détective": "・Détective", 
+        "Petite_fille": "・Petite fille",
+        "Boucher": "・Boucher", 
+        "Salvateur": "・Salvateur", 
+        "Sorcière": "・Sorcière", 
+        "Chaman": "・Chaman", 
+        "Médium": "・Médium", 
+        "Dompteur": "・Dompteur",
+        "Chasseur_aveugle": "・Chasseur aveugle", 
+        "Chasseur": "・Chasseur", 
+        "Tireur_d'elite": "・Tireur d'elite", 
+        "Corbeau": "・Corbeau", 
+        "Momie": "・Momie", 
+        "Hacker": "・Hacker", 
+        "Brouilleur": "・Brouilleur", 
+        "Chien_loup": "・Chien loup",
+        "Enfant_sauvage": "・Enfant sauvage",
+        "Servante dévouée": "・Servante dévouée", 
+        "Voleur": "・Voleur", 
+        "Traqueur_Résurrecteur": "・Traqueur Résurrecteur", 
+        "Assassin": "・Assassin", 
+        "Toxico": "・Toxico", 
+        "Joueur de Flûte": "・Joueur de flûte", 
+        "flûtistes": "・flûtistes",
+        "Loup_garou": "・Loup-garou", 
+        "Loup_garou_blanc": "・Loup garou blanc"
+    },
+
+    "ROLE_LG": {},
+
+    "WIN_LG": {
+        "égalité" : {
+            "title": "⚖️ • Égalité" , 
+            "desc": "Aucune personne remporte cette partie.", 
+            "Winner": 0
+        },
+
+        "couple" : {
+            "title": "♥ • Le couple" , 
+            "desc": "Le couple remporte cette partie.", 
+            "Winner": "du __couple__"
+        },
+
+        "village" : {
+            "title": "🏘 • Le village" , 
+            "desc": "Le village remporte cette partie.", 
+            "Winner": "du __village__"
+        },
+
+        "loups" : {
+            "title": "🐺 • Les loups" , 
+            "desc": "Les loups remportent cette partie.", 
+            "Winner": "des __loups__"
+        },
+
+        "toxico" : {
+            "title": "🚬 • Le toxico" , 
+            "desc": "Le toxico remporte cette partie.", 
+            "Winner": "du __toxico__"
+        },
+
+        "lgb" : {
+            "title": "🐺 • Le loup garou blanc" , 
+            "desc": "Le loup garou blanc remporte cette partie.", 
+            "Winner": "du __loup garou blanc__"
+        },
+
+        "jdf" : {
+            "title": "🎺 • Le joueur de flûte" , 
+            "desc": "Le joueur de flûte remporte cette partie.", 
+            "Winner": "du __joueur de flûte__"
+        },
+
+        "assassin" : {
+            "title": "🗡️• L'assassin" , 
+            "desc": "L'assassin remporte cette partie.", 
+            "Winner": "de l'__assassin__"
+        },
+
+        "ange" : {
+            "title": "😇 • L'ange" , 
+            "desc": "L'ange remporte cette partie.", 
+            "Winner": "de l'__ange__"
+        },
+
+        "ange2" : {
+            "title": "😇 • L'ange" , 
+            "desc": "A la suite du vote, l'ange ce suicide en voyant qu'il y'a eu aucun mort le premier jour, l'ange remporte cette partie.", 
+            "Winner": "de l'__ange__"
+        },
+
+        "fou" : {
+            "title": "🃏 • Du fou" , 
+            "desc": "Le fou remporte cette victoire.", 
+            "Winner": "du __fou__"
+        },
+
+        "traqueur" : {
+            "title": "🧟‍♂️ • Du traqueur résurrecteur" , 
+            "desc": "Le traqueur résurrecteur remporte cette partie.", 
+            "Winner": "du __traqueur résurrecteur__"
+        },
+    },
+}
