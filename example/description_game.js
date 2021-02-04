d2b40bbefc19ae0e93a1449e55c892c73fe532f1
NewGlace <35409106+NewGlace@users.noreply.github.com>
2021-02-04 22:35:09 +0100

    Update description_game.js

--- example/description_game.js
+++ example/description_game.js
@@ -119,4 +119,271 @@ const WerewolfTemplate = {
             "Winner": "du __traqueur résurrecteur__"
         },
     },
+    
+    "ROLE_LG": {
+        "Enfant_sauvage": {"name": "👦 • L'enfant sauvage",
+            "desc_village": "L'enfant sauvage a **30** secondes pour choisir son idole.",
+            "desc_channel": "Vous avez **30** secondes pour choisir votre idole:\nxxx", //Don't edit "xxx" and don't place other "xxx"
+            "role": "Enfant Sauvage",
+            "sleep": "L'enfant sauvage s'est endormi."
+        },
+        "Chien_loup": {"name": "🐶 • Le chien loup",
+            "villa": "Le chien loup a **30** secondes pour choisir un camp.",
+            "chann": "Vous disposez de **30** secondes pour choisir votre camp:\n🐺 Loup Garou \n🧒 Village.",
+            "role" : "Chien Loup",
+            "sleep": "Le chien loup s'est endormi."
+        },
+
+        "Cupidon": {"name": "💘 • Le cupidon",
+            "villa": "Le cupidon a **30** secondes pour choisir ses amoureux.",
+            "chann": "Vous pouvez choisir vos amoureux quant vous le souhaitez jusqu'au jour 2:\nxxx",
+            "role" : "Cupidon",
+            "sleep": "Le cupidon s'est endormi."
+        },
+
+        "Voleur": {"name": ":spy: • Le voleur",
+            "villa": "Le voleur a **30** secondes pour échanger son rôle.",
+            "chann": "Vous disposez de 30 secondes pour échanger votre rôle avec un des joueurs:\nxxx",
+            "role" : "Voleur",
+            "sleep": "Le voleur s'est endormi."
+        },
+
+        "Hacker": {"name": "💻 • Le hacker",
+            "villa": "Le hacker a **30** secondes pour choisir de passer une nuit ou un jour.",
+            "chann": "Vous avez **30** secondes pour choisir de passer une nuit ou un jour \n🌕 Passer la nuit.\n🌞 Passer le jour.",
+            "role" : "Hacker",
+            "sleep": "Le hacker s'est endormi."
+        },
+
+        "Brouilleur": {"name": "🛰 • Le brouilleur",
+            "villa": "Le brouilleur a **30** secondes pour choisir de brouiller les votes nocturne.",
+            "chann": "Vous avez **30** secondes pour choisir de brouiller les votes nocturne.",
+            "role" : "Brouilleur",
+            "sleep": "Le brouilleur s'est endormi."
+        },
+
+        "Oracle": {"name": "👴 • L'oracle",
+            "villa": "L'oracle a **30** secondes pour regarder 2 cartes.",
+            "chann": "Vous avez **30** secondes pour regarder 2 cartes:\nxxx",
+            "role" : "Oracle",
+            "sleep": "l'oracle s'est endormi."
+        },
+
+        "Oracle_oeil": {"name": "👴 • L'oracle au 3 ème œil",
+            "villa": "L'oracle a **30** secondes pour regarder 2 cartes.",
+            "chann": "Vous avez **30** secondes pour regarder 2 cartes:\nxxx",
+            "role" : "Oracle",
+            "sleep": "l'oracle s'est endormi."
+        },
+
+        "Voyante": {"name": "👁 • La voyante",
+            "villa": "La voyante a **30** secondes pour regarder la carte d'un joueur.",
+            "chann": "Vous avez **30** secondes pour regarder la carte d'un joueur:\nxxx ",
+            "role" : "Voyante",
+            "sleep": "La voyante s'est endormie."
+        },
+
+        "Voyante_bavard": {"name": "👁 • La voyante Bavard",
+            "villa": "La voyante a **30** secondes pour regarder la carte d'un joueur.",
+            "chann": "Vous avez **30** secondes pour regarder la carte d'un joueur:\nxxx ",
+            "role" : "Voyante",
+            "sleep": "La voyante s'est endormie."
+        },
+
+        "Renard": {"name": "🦊 • Le renard",
+            "villa": "Le renard a **30** secondes pour renifler la carte d'un joueur.",
+            "chann": "Vous avez **30** secondes pour renifler la carte d'un joueur:\nxxx",
+            "role" : "Renard",
+            "sleep": "Le renard s'est endormi."
+        },
+
+        "Montreur": {"name": "🐻 • Le montreur d'ours",
+            "villa": "Le montreur d'ours a **30** secondes pour choisir deux personnes.",
+            "chann": "Vous avez **30** secondes pour choisir vos 2 personnes\nxxx",
+            "role" : "Montreur d'ours",
+            "sleep": "Le montreur d'ours s'est endormi."
+        },
+
+        "Détective": {"name": "🔎 • Le détective",
+            "villa": "Le détective a **30** secondes pour choisir 2 joueurs.",
+            "chann": "Vous avez **30** secondes pour choisir 2 joueurs:\nxxx",
+            "role" : "Détective",
+            "sleep": "Le détective s'est endormi"
+        },
+
+        "Salvateur": {"name": "🛡 • Le salvateur",
+            "villa": "Le salvateur a **30** secondes pour protéger un joueur.",
+            "chann": "Vous avez **30** secondes pour protéger un joueur d'une attaque nocturne:\nsss",
+            "role" : "Salvateur",
+            "sleep": "Le salvateur s'est endormi."
+        },
+
+        "Loups": {"name": "🐺 • Les loups",
+            "villa": "Les loups ont **60** secondes pour voter leur victime.",
+            "chann": "Vous avez **60** secondes pour choisir une victime:\nlll",
+            "role" : "Loups",
+            "sleep": "Les loups se sont endormis."
+        },
+
+        "Boucher": {"name": "🥩 • Le boucher",
+            "villa": "Le boucher a **30** secondes pour choisir d'échanger la victime des Loups par un morceau de viande.",
+            "chann": "Vous avez **30** secondes pour choisir de détourné les loups sur une fausse victime ou de vous rendormir.",
+            "role" : "Boucher",
+            "sleep": "Le boucher s'est endormi."
+        },
+
+        "Infect_père": {"name": "🐺 • L'infect père des loups",
+            "villa": "L'infect père des loups a **30** secondes pour infecter un joueur.",
+            "chann": "Vous avez **30** secondes pour choisir d'infecter ",
+            "role" : "Infect père des loups",
+            "sleep": "L'infect père des loups s'est endormi."
+        },
+
+        "Grand_Méchant_Loup": {"name": "🐺 • Le grand méchant loup",
+            "villa": "Le grand méchant loup a **30** secondes pour choisir une victime.",
+            "chann": "Vous avez **30** secondes pour choisir une victime:\nlll",
+            "role" : "Grand méchant loup",
+            "sleep": "Le grand méchant loup s'est endormi."
+        },
+
+        "Loup_Vengeurs": {"name": "🐺 • Le loup vengeurs",
+            "villa": "Le loup vengeur a **30** secondes pour se venger de la mort d'un de ses loups.",
+            "chann": "Vous avez **30** secondes pour choisir une victime:\nlll",
+            "role" : "Loup vengeurs",
+            "sleep": "Le loup vengeurs s'est endormi."
+        },
+
+        "Loup_garou_blanc": {"name": "🐺 • Le loup garou blanc",
+            "villa": "Le loup garou blanc a **30** secondes pour tuer un loup.",
+            "chann": "Vous avez **30** secondes pour choisir une victime\nlgb",
+            "role" : "Loup Garou blanc",
+            "sleep": "Le loup garou blanc s'est endormi."
+        },
+
+        "Assassin": {"name": "🗡 • L'assassin",
+            "villa": "L'assassin a **30** secondes pour tuer sa victime.",
+            "chann": "Vous avez **30** secondes pour choisir une victime:\nxxx",
+            "role" : "Assassin",
+            "sleep": "L'assassin s'est endormi"
+        },
+
+        "Traqueur_résurrecteur": {"name": "🧟‍♂️ • Le traqueur résurrecteur",
+            "villa": "Le traqueur résurrecteur a **30** secondes pour tuer sa/ses victime(s).",
+            "chann": "Vous avez **30** secondes pour choisir une victime:\nxxx",
+            "role" : "Traqueur résurrecteur",
+            "sleep": "Le traqueur résurrecteur s'est endormi"
+        },
+
+        "Joueur_de_flûte": {"name": "🎺 • Le joueur de flûte",
+            "villa": "Le joueur de flûte a **30** secondes pour choisir les nouveaux flûtistes.",
+            "chann": "Vous avez **30** secondes pour choisir vos 2 nouveaux flûtistes:\nfff",
+            "role" : "Joueur de Flûte",
+            "sleep": "Le joueur de flûte s'est endormi."
+        },
+
+        "Toxico": {"name": "🚬 • Le toxico",
+            "villa": "Le toxico a **30** secondes pour choisir les ses victimes.",
+            "chann": "Vous avez **30** secondes pour choisir vos victimes. \n💀 Permet de tuer tous les toxicos, attention si vous commencez à rendre des gens toxico vous ne pourrez pas utiliser cette fonction. \nttt",
+            "role" : "Toxico",
+            "sleep": "Le toxico s'est endormi."
+        },
+
+        "Sorcière": {"name": "⚗ • La sorcière",
+            "villa": "La sorcière a **30** secondes pour utiliser l'une de ses potions.",
+            "chann": "",
+            "role" : "Sorcière",
+            "sleep": "La sorcière s'est endormie."
+        },
+
+        "Momie": {"name": "<:momi:619185764286070784> • La momie",
+            "villa": "La momie a **30** secondes pour momifier une personne.",
+            "chann": "Vous avez **30** secondes pour momifier une personne cela l'empêchera de voter\nxxx",
+            "role" : "Momie",
+            "sleep": "La momie s'est endormi."
+        },
+
+        "Corbeau": {"name": "<:corbeau:619191960392957971> • Le corbeau",
+            "villa": "Le corbeau a **30** secondes pour choisir une personne.",
+            "chann": "Vous avez **30** secondes pour choisir une personne\nxxx",
+            "role" : "Corbeau",
+            "sleep": "Le corbeau s'est endormi."
+        },
+
+        "Médium": {"name": "🔮 • Le médium",
+            "villa": "Le médium a **30** secondes pour parler au mort.",
+            "chann": "Vous avez **30** secondes pour communiquer avec les morts.",
+            "role" : "Médium",
+            "sleep": "Le médium s'est endormi."
+        },
+
+        "Chaman": {"name": "🔮 • Le chaman",
+            "villa": "Le chaman a **30** secondes pour parler au mort.",
+            "chann": "Vous avez **30** secondes pour communiquer avec les morts.",
+            "role" : "Chaman",
+            "sleep": "Le chaman s'est endormi."
+        },
+
+        "Dompteur": {"name": "🦁 • Le dompteur",
+            "villa": "",
+            "chann": "Vous avez **30** secondes pour choisir un loup mort à apprivoiser.\nddd",
+            "role" : "Dompteur",
+            "sleep": ""
+        },
+
+        "Tireur": {"name": "🔫 • Le tireur d'élite",
+            "villa": "",
+            "chann": "Vous avez **30** secondes pour choisir une personne à tuer.\nxxx",
+            "role" : "Tireur d'élite",
+            "sleep": ""
+        },
+
+        "Loup_Chaman": {"name": "🐺 • Le loup chaman",
+            "villa": "",
+            "chann": "Vous avez **30** secondes pour choisir une personne à chamaniser.\nlll",
+            "role" : "Loup chaman",
+            "sleep": ""
+        },
+
+        "Chasseur": {"name": "🔫 • Le chasseur",
+            "villa": "le chasseur a **30** secondes pour utiliser sa dernière balle.",
+            "chann": "Vous avez **30** secondes pour choisir une victime.\nxxx",
+            "role" : "Chasseur",
+            "sleep": "le chasseur meurt"
+        },
+
+        "Chasseur_aveugle": {"name": "🔫 • Le chasseur aveugle",
+            "villa": "le chasseur a **30** secondes pour utiliser sa dernière balle.",
+            "chann": "Vous avez **30** secondes pour choisir une victime.\nxxx",
+            "role" : "Chasseur",
+            "sleep": "le chasseur meurt"
+        },
+
+        "Soeurs": {"name": "👭 • Les sœurs",
+            "villa": "",
+            "chann": "Vous pouvez discuter.",
+            "role" : "Sœurs",
+            "sleep": "Vous vous endormez."
+        },
+
+        "Jumeaux": {"name": "👬 • Les jumeaux",
+            "villa": "",
+            "chann": "Vous pouvez discuter.",
+            "role" : "Jumeaux",
+            "sleep": "Vous vous endormez."
+        },
+
+        "Petite_fille": {"name": "👧 • La petite fille",
+            "villa": "La petite fille a **15** secondes pour choisir d'ouvrir les yeux.",
+            "chann": "vous avez **15** secondes pour choisir d'ouvrir les yeux.",
+            "role" : "Petite fille",
+            "sleep": "La petite fille s'est endormie."
+        },
+
+        "Servante_dévouée": {"name": "👩 • La servante dévouée",
+            "villa": "La servante dévoué à **15** secondes pour choisir d'échanger leurs cartes.",
+            "chann": "vous avez **15** secondes pour choisir d'échanger votre carte avec xxx.",
+            "role" : "Servante dévouée",
+            "sleep": "La servante dévouée s'est endormie."
+        }
+    }
 }
